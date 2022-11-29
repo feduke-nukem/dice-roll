e7225fb47d5e3338017987d82b1b0f3db0095fe6
Федор Благодырь <bagotir@gmail.com>
2022-11-29 10:39:34 +0500

    finished first lesson block

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,19 +2,35 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var diceImages =
+        listOf(
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6,
+        )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        // New binding approach
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val rollButton = binding.rollButton
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        binding.rollButton.setOnClickListener {
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val newSrc = diceImages[Random.nextInt(6)]
+        binding.diceImage.setImageResource(newSrc)
     }
 }
